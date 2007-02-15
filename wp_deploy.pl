031a2a9965a299af8e4a59fb121b4e8109c184ef
Daniel Westermann-Clark <dwc@ufl.edu>
2007-02-15 21:39:39 +0000

    r10267@adora (orig r13):  dwc | 2006-06-15 14:30:59 -0400  Add default list of executables

--- wp_deploy.pl
+++ wp_deploy.pl
@@ -24,6 +24,63 @@ our @DEFAULT_RSYNC_EXCLUDES = qw(
 );
 our $DEFAULT_SHEBANG  = '#!/usr/local/bin/php';
 our @DEFAULT_EXECUTABLES = qw(
+    index.php
+    wp-atom.php
+    wp-comments-post.php
+    wp-commentsrss2.php
+    wp-feed.php
+    wp-links-opml.php
+    wp-login.php
+    wp-mail.php
+    wp-pass.php
+    wp-rdf.php
+    wp-register.php
+    wp-rss2.php
+    wp-rss.php
+    wp-trackback.php
+    xmlrpc.php
+);
+push @DEFAULT_EXECUTABLES, map { File::Spec->join('wp-admin', $_) } qw(
+    bookmarklet.php
+    categories.php
+    cat-js.php
+    edit-comments.php
+    edit-form-ajax-cat.php
+    edit-pages.php
+    edit.php
+    import.php
+    index.php
+    inline-uploading.php
+    install-helper.php
+    install.php
+    link-add.php
+    link-categories.php
+    link-import.php
+    link-manager.php
+    list-manipulation.php
+    moderation.php
+    options-discussion.php
+    options-general.php
+    options-misc.php
+    options-permalink.php
+    options.php
+    options-reading.php
+    options-writing.php
+    page-new.php
+    plugin-editor.php
+    plugins.php
+    post.php
+    profile.php
+    profile-update.php
+    setup-config.php
+    sidebar.php
+    templates.php
+    theme-editor.php
+    themes.php
+    update-links.php
+    upgrade.php
+    user-edit.php
+    users.php
 );
 
 
@@ -101,9 +158,25 @@ sub stage {
     _copy([ $www, $configuration ], $stage, \@args);
 }
 
-# TODO
 sub add_shebang {
     my ($shebang, $executables, $directory) = @_;
+
+    foreach my $executable (@$executables) {
+        $executable = File::Spec->join($directory, $executable);
+        next unless -f $executable;
+
+        my $fh;
+
+        # Slurp the contents
+        open $fh, '<', $executable or die "Error opening $executable: $!";
+        my $content = do { local $/; <$fh> };
+        close $fh;
+
+        # Add the shebang
+        open $fh, '>', $executable or die "Error opening $executable: $!";
+        print $fh, $shebang, "\n", $content;
+        close $fh;
+    }
 }
 
 # TODO
