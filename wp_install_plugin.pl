212e7477a91261600eebc40883d1b5d0d54f3726
Daniel Westermann-Clark <dwc@ufl.edu>
2007-02-15 21:39:29 +0000

    r10257@adora (orig r3):  dwc | 2006-06-01 14:36:31 -0400  Use URI module to build plugin URIs

--- wp_install_plugin.pl
+++ wp_install_plugin.pl
@@ -4,13 +4,14 @@ use strict;
 use warnings;
 use File::Basename;
 use File::Spec;
+use URI;
 
 
 ##
 ## Globals
 ##
 
-our $DEFAULT_SVN_ROOT = 'svn+ssh://dev.webadmin.ufl.edu/var/svn/wordpress/plugins';
+our $DEFAULT_SVN_REPO = URI->new('svn+ssh://dev.webadmin.ufl.edu/var/svn/wordpress/plugins');
 
 
 ##
@@ -19,20 +20,23 @@ our $DEFAULT_SVN_ROOT = 'svn+ssh://dev.webadmin.ufl.edu/var/svn/wordpress/plugin
 
 main(@ARGV);
 sub main {
-    my ($plugin_url, $wordpress_root) = @_;
+    my ($plugin_uri, $wordpress_root) = @_;
 
     die "$0 PLUGIN_URL WORDPRESS_ROOT\n"
-        unless $plugin_url and $wordpress_root;
+        unless $plugin_uri and $wordpress_root;
 
-    if ($plugin_url !~ /:\/\//) {
-        $plugin_url = "$DEFAULT_SVN_ROOT/$plugin_url/trunk";
+    if ($plugin_uri !~ /:\/\//) {
+        my $plugin_name = $plugin_uri;
+
+        $plugin_uri = $DEFAULT_SVN_REPO->clone;
+        $plugin_uri->path_segments($plugin_uri->path_segments, $plugin_name, 'trunk');
     }
 
     my $plugin_root = File::Spec->join($wordpress_root, 'wp-content', 'plugins');
     die "Plugin directory [$plugin_root] is not writable\n"
         unless -d $plugin_root;
 
-    my $plugin_directory = File::Spec->join($plugin_root, basename(dirname($plugin_url)));
+    my $plugin_directory = File::Spec->join($plugin_root, basename(dirname($plugin_uri->path)));
     if (-d $plugin_directory) {
         my $old_plugin_directory = "$plugin_directory.old";
         warn "Plugin directory exists; renaming to [$old_plugin_directory]\n";
@@ -41,6 +45,6 @@ sub main {
         rename $plugin_directory, $old_plugin_directory;
     }
 
-    print "Exporting [$plugin_url] to [$plugin_directory]...\n";
-    system('svn', 'export', $plugin_url, $plugin_directory);
+    print "Exporting [$plugin_uri] to [$plugin_directory]...\n";
+    system('svn', 'export', $plugin_uri, $plugin_directory);
 }
