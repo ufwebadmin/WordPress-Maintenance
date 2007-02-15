162def80eafe97e7d7b6dda782d8b47adb9ff8ed
Daniel Westermann-Clark <dwc@ufl.edu>
2007-02-15 21:40:05 +0000

    r10293@adora (orig r39):  dwc | 2006-07-10 15:08:45 -0400  Cleanup and make acceptable_files and rejected_uris configuration options

--- templates/wp-content/wp-cache-config.php
+++ templates/wp-content/wp-cache-config.php
@@ -1,21 +1,15 @@
 <?php
-/*
-WP-Cache Config Sample File
-
-See wp-cache.php for author details.
-*/
-
-$cache_enabled = [% wordpress.wp_cache ? 'true' : 'false' %]; //Added by WP-Cache Manager
-$cache_max_time = 3600; //in seconds
-$use_flock = true; // Set it true or false if you know what to use
+$cache_enabled = [% wordpress.wp_cache.enabled ? 'true' : 'false' %];
+$cache_max_time = 3600; // seconds
+$use_flock = true;
 $cache_path = ABSPATH . 'wp-content/wp-cache/';
 $file_prefix = 'wp-cache-';
 $known_headers = array("Last-Modified", "Expires", "Content-Type", "X-Pingback", "ETag", "Cache-Control", "Pragma");
 
-// Array of files that have 'wp-' but should still be cached 
-$cache_acceptable_files = array( 'wp-atom.php', 'wp-comments-popup.php', 'wp-commentsrss2.php', 'wp-links-opml.php', 'wp-locations.php', 'wp-rdf.php', 'wp-rss.php', 'wp-rss2.php');
+// Array of files that have 'wp-' but should still be cached
+$cache_acceptable_files = array('[% wordpress.wp_cache.acceptable_files.join("', '") %]');
 
-$cache_rejected_uri = array('wp-');
+$cache_rejected_uri = array('[% wordpress.wp_cache.rejected_uris.join("', '") %]');
 $cache_rejected_user_agent = array ( 0 => 'bot', 1 => 'ia_archive', 2 => 'slurp', 3 => 'crawl', 4 => 'spider');
 
 // Just modify it if you have conflicts with semaphores
@@ -33,5 +27,4 @@ if (!class_exists('CacheMeta')) {
 if ( '/' != substr($cache_path, -1)) {
 	$cache_path .= '/';
 }
-
 ?>
