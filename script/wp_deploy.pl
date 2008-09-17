ebbc1bc922f40e5e68105fceb63717aee4604ff2
Daniel Westermann-Clark <dwc@ufl.edu>
2008-09-17 19:22:28 +0000

    Fix distribution name

--- script/wp_deploy.pl
+++ script/wp_deploy.pl
@@ -35,7 +35,7 @@ main(@ARGV);
 sub main {
     my $source_directory   = File::Spec->curdir;
     my $environment        = 'dev';
-    my $template_directory = dist_dir('WordPress::Maintenance');
+    my $template_directory = dist_dir('WordPress-Maintenance');
     my $checkout           = 0;
     my $cleanup            = 1;
     my $help               = 0;
