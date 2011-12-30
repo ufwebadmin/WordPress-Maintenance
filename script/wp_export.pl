1ed1211281312e74cf26840bae723ab6ccbe2b6d
Daniel Westermann-Clark <dwc@ufl.edu>
2011-12-30 10:39:41 -0500

    Update wp_export.pl to make base URL more sensible

--- script/wp_export.pl
+++ script/wp_export.pl
@@ -15,33 +15,40 @@ use WordPress::Maintenance::Mechanize;
 main(@ARGV);
 sub main {
     my $base_url;
-    my $site;
     my $username;
     my $password;
     my $output_directory = File::Spec->curdir;
-    my $help             = 0;
+    my $output_filename;
+    my $help = 0;
     die usage() unless GetOptions(
         'base|b=s'     => \$base_url,
-        'site|s=s'     => \$site,
         'username|u=s' => \$username,
         'password|p=s' => \$password,
         'output|o=s'   => \$output_directory,
+        'filename|f=s' => \$output_filename,
         'help|h'       => \$help,
     );
     print usage() and exit() if $help;
     die usage() unless $username and $password and $base_url;
 
-    my $path = $site || '/';
-    my $name = $site || 'default';
+    if ($base_url !~ m|^https?://|) {
+        $base_url = 'http://' . $base_url;
+    }
 
     my $url = URI->new($base_url);
-    $url->path($path);
 
     my $mech = WordPress::Maintenance::Mechanize->new({ url => $url });
     $mech->login($username, $password);
 
-    my $filename = File::Spec->join($output_directory, $name . '.xml');
-    $mech->export_site($filename);
+    unless ($output_filename) {
+        $output_filename = $url;
+        $output_filename =~ s|^https?://||;
+        $output_filename =~ s|/$||;
+        $output_filename =~ s|/|_|g;
+    }
+
+    my $output_file = File::Spec->join($output_directory, $output_filename . '.xml');
+    $mech->export_site($output_file);
 }
 
 
@@ -51,13 +58,21 @@ sub main {
 
 sub usage {
     return <<"END_OF_USAGE";
-Usage: $0 [OPTION]... < list_of_site_names.txt
+Usage: $0 [OPTION]...
 
 Available options:
-  -b, --base        The base WordPress URL
+  -b, --base        The base WordPress URL, e.g. http://example.com/wordpress/
   -u, --username    The WordPress admin username
   -p, --password    The WordPress admin password
   -o, --output      The directory in which to store export files
+                      (default: current directory)
+  -f, --filename    The filename to use for the export file
+                      (default: based on base WordPress URL)
   -h, --help        Print this help screen and exit
+
+For the base WordPress URL, specify the location from which
+wp-login.php, wp-admin, etc. can be accessed. Do not include anything
+like wp-login.php after the main site URL. For multisite setups use
+the site URL, not the network URL.
 END_OF_USAGE
 }
